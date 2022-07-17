270311ead1c91ccc0be8ffbd7f9861b17c4bdd3a
dular88 <dular88@gmail.com>
2022-07-17 15:16:37 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Coding Skul is a Raipur Based Web Development and Technical Training and Internship Provider. Our aim is to make people skillfull according to industry demand. We provides complete practical oriented training and Internship in Raipur Chhattisgarh." />
+    <meta name="keywords" content="coding skul,Web Development and Technical Internship Provider in Raipur, web designing training center in raipur, web development training in raipur, seo training institute in raipur" />
+    <title>Coding Skul</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        .photo{
+            height:500px;
+        }
+       
+    </style>
+</head>
+<body>
+<?php include("menu.php"); ?>
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="photo/Digital-Marketing.png" class="d-block w-100 photo" alt="Digital Marketing">
+          </div>
+          <div class="carousel-item">
+            <img src="photo/photo.png" class="d-block w-100 photo" alt="Photo and Video">
+          </div>
+          <div class="carousel-item">
+            <img src="photo/Web-Development.png" class="d-block w-100 photo" alt="Web Developement">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div class="row m-2">
+        <h1>About Coding Skul</h1>
+        <div class="col-sm-12">
+            <p class="text-justify">
+                Coding Skul is a Raipur Based Web Development and Technical Training and Internship Provider. Our aim is to make people skillfull according to industry demand. We provides complete practical oriented training and Internship in Raipur Chhattisgarh.
+            </p>
+        </div>
+      </div>
+      <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+          <p class="text-center">All &copy; 2022 are reserved by <a href="https://bdidi.in">bdidi.in</a></p>
+        </div>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ service.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Coding Skul is a Raipur Based Web Development and Technical Training and Internship Provider. Our aim is to make people skillfull according to industry demand. We provides complete practical oriented training and Internship in Raipur Chhattisgarh." />
+    <meta name="keywords" content="coding skul,Web Development and Technical Internship Provider in Raipur, web designing training center in raipur, web development training in raipur, seo training institute in raipur" />
+    <title>Coding Skul</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        .justify{
+            text-align: justify;
+        }
+        .photo{
+            height:500px;
+        }
+       
+    </style>
+</head>
+<body>
+<?php include("menu.php"); ?>
+      <div class="row m-2">
+        <div class="col-sm-12">
+            <div class="card mb-3">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="photo/Digital-Marketing.png" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Digital-Marketing</h5>
+                      <p class="card-text justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card mb-3">
+                <div class="row g-0">
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Photo and Video</h5>
+                      <p class="card-text justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                    <img src="photo/photo.png" class="img-fluid rounded-start" alt="...">
+                  </div>
+                </div>
+              </div>
+              <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="photo/Web-Development.png" class="img-fluid rounded-start" alt="...">
+                      </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title">Web Developement</h5>
+                      <p class="card-text justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      </div>
+                  </div>
+                  
+                </div>
+              </div>
+        </div>
+      </div>
+      
+      <footer class="footer mt-auto py-3 bg-light">
+        <div class="container">
+          <p class="text-center">All &copy; 2022 are reserved by <a href="https://bdidi.in">bdidi.in</a></p>
+        </div>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
